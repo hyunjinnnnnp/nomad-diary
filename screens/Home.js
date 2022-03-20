0c2cff9f519ffdd89ee0f017bd2077aeeb8bf1d4
hyunjinnnnnp <imhjinnnnn@gmail.com>
2022-03-20 12:59:57 +0900

    DB Listeniner

--- screens/Home.js
+++ screens/Home.js
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components/native";
 import { Ionicons } from "@expo/vector-icons";
 import colors from "../colors";
+import { useDB } from "../context";
+import { FlatList } from "react-native";
 
 const View = styled.View`
   flex: 1;
@@ -28,13 +30,61 @@ const Btn = styled.TouchableOpacity`
   elevation: 5; // for android
 `;
 
-const Home = ({ navigation: { navigate } }) => (
-  <View>
-    <Title>My journal</Title>
-    <Btn onPress={() => navigate("Write")}>
-      <Ionicons name="add" color="white" size={40} />
-    </Btn>
-  </View>
-);
+const Record = styled.View`
+  background-color: ${colors.cardColor};
+  flex-direction: row;
+  align-items: center;
+  padding: 10px 20px;
+  border-radius: 10px;
+`;
+const Emotion = styled.Text`
+  font-size: 24px;
+  margin-right: 10px;
+`;
+
+const Message = styled.Text`
+  font-size: 18px;
+  font-weight: 400;
+`;
+const Separator = styled.View`
+  height: 10px;
+`;
+
+const Home = ({ navigation: { navigate } }) => {
+  const realm = useDB();
+  const [feelings, setFeelings] = useState();
+  useEffect(() => {
+    const feelings = realm.objects("Feeling");
+    setFeelings(feelings);
+    feelings.addListener(() => {
+      const feelings = realm.objects("Feeling");
+      setFeelings(feelings);
+    });
+    return () => {
+      feelings.removeAllListeners();
+      //will be called when the component is unmounted, so it allows us to remove event listeners to prevent memory leaks.
+    };
+  }, []);
+  return (
+    <View>
+      <Title>My journal</Title>
+      <FlatList
+        data={feelings}
+        contentContainerStyle={{ paddingVertical: 10 }}
+        ItemSeparatorComponent={Separator}
+        key={(feelings) => feelings._id + ""}
+        renderItem={({ item }) => (
+          <Record>
+            <Emotion>{item.emotion}</Emotion>
+            <Message>{item.message}</Message>
+          </Record>
+        )}
+      />
+      <Btn onPress={() => navigate("Write")}>
+        <Ionicons name="add" color="white" size={40} />
+      </Btn>
+    </View>
+  );
+};
 
 export default Home;
